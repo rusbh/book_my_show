cf2c5eb9af150aab851639837fc26053e2c0489e
Rushabh Modi <rushabhmodi2552@gmail.com>
2024-06-07 17:31:18 +0530

    validations & general impros

--- app/models/theater_admin.rb
+++ app/models/theater_admin.rb
@@ -5,7 +5,8 @@ class TheaterAdmin < ApplicationRecord
   belongs_to :theater
   belongs_to :user
 
-  validates :user_id, uniqueness: {scope: :theater_id, message: "this admin is already assigned to this theater."}
+  validates :user_id, uniqueness: { scope: :theater_id, message: 'this admin is already assigned to this theater.' }
+  validates :user_id, uniqueness: { message: 'Admin can only be assigned to one theater' }
 
   private
 
